14f12414d5e955a28bbd2731b880ce63b1219aaf
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-04-30 16:17:42 -0500

    even-function done

---
+++ main.js
@@ -0,0 +1,28 @@
+let painter = {
+  tools: ["brush", "roller", "caulk", "sandpaper", "dropcloth"],
+  uniform: "white overalls",
+  cost_per_hour: 25.00,
+  insured: true,
+  paint: function() {
+    return this.cost_per_hour * workHours;
+}
+}
+/*Add an action that the painter can perform 
+( a property whose value is a function ).
+Have her take that action by execucting the method you created.
+BONUS: Add a method that allows us to add new tools for the painter.
+*/
+
+
+let workHours = 8;
+painter.paint();
+console.log(painter.paint())
+
+const tools = ["brush", "roller", "caulk", "sandpaper", "dropcloth"];
+//tools.push("paint", "ladder");//
+const count = tools.push('paint');
+console.log(count);
+console.log(tools);
+
+
+
